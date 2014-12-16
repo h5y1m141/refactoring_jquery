f8e80f4a85cd3e381bb7bd2427d647170cdcf3d1
HiroshiOyamada <h5y1m141@gmail.com>
2014-12-16 13:28:13 +0900

    Viewを初期化するmain.jsを修正

--- js/main.js
+++ js/main.js
@@ -1,21 +1,23 @@
 $(document).ready(function() {
-  var $form = $('.todoForm');
-  var $input = $form.find('input[type="text"]');
-  var $list = $('.todoList');
-  console.log($form);
-  $form.submit(function(e) {
+  new TodoFormView($('.todoForm'));
+  new TodoListView($('.todoList'));
+  // var $form = $('.todoForm');
+  // var $input = $form.find('input[type="text"]');
+  // var $list = $('.todoList');
+  // console.log($form);
+  // $form.submit(function(e) {
 
-    e.preventDefault();
-    var text = $input.val();
+  //   e.preventDefault();
+  //   var text = $input.val();
 
-    var html = '<li><input type="checkbox">' + text + '</li>';
-    var $li = $(html);
+  //   var html = '<li><input type="checkbox">' + text + '</li>';
+  //   var $li = $(html);
 
-    $li.find('input[type="checkbox"]').change(function() {
-      $(this).closest('li').toggleClass('complete');
-    });
+  //   $li.find('input[type="checkbox"]').change(function() {
+  //     $(this).closest('li').toggleClass('complete');
+  //   });
 
-    $list.append($li);
-    $input.val('');
-  });
+  //   $list.append($li);
+  //   $input.val('');
+  // });
 });
