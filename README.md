203fb919df45122f58584b3424256acf13092d57
HiroshiOyamada <h5y1m141@gmail.com>
2014-12-16 11:42:12 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## このリポジトリについて
+
+[フロントエンドJavaScriptにおける設計とテスト](http://hokaccha.github.io/slides/javascript_design_and_test/)というとてもわかりやすいスライドがあります。
+
+それを実際に写経しながら、jQueryベースのよくあるようなJavaScriptのリファクタリングをしてみました
---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  var $form = $('.todoForm');
+  var $input = $form.find('input[type="text"]');
+  var $list = $('.todoList');
+  console.log($form);
+  $form.submit(function(e) {
+
+    e.preventDefault();
+    var text = $input.val();
+
+    var html = '<li><input type="checkbox">' + text + '</li>';
+    var $li = $(html);
+
+    $li.find('input[type="checkbox"]').change(function() {
+      $(this).closest('li').toggleClass('complete');
+    });
+
+    $list.append($li);
+    $input.val('');
+  });
+});
