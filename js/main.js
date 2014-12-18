62e7beaf95986affe539a4ef39a52a3661684671
HiroshiOyamada <h5y1m141@gmail.com>
2014-12-19 07:03:09 +0900

    ここをクリックという文章をクリックした時にレイヤーが表示されるようまずは実装

--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
 $(document).ready(function() {
   new TodoFormView($('.todoForm'));
   new TodoListView($('.todoList'));
+  $("#subTitle").click(function(e){
+    var textarea, offset;
+	  console.log(this.innerHTML);
+    offset = $(this).offset();
+    console.log(offset);
+    textarea = $('<textarea>').css({
+      display: 'block',
+      position: 'absolute',
+      left: offset.left,
+      top: offset.top,
+      width: 300,
+      height: 100,
+      fontSize: 14,
+      fontFamily: "'Open Sans', Helvetica, Arial, sans-serif",
+      zIndex: 1,
+      background: 'rgba(255,255,255,0.9)'
+    }).val(this.innerHTML);
+    return $('body').append(textarea);
+
+  });
   // var $form = $('.todoForm');
   // var $input = $form.find('input[type="text"]');
   // var $list = $('.todoList');
