62c02f852c8b4091ce1b2041a066f4e93f1bf936
HiroshiOyamada <h5y1m141@gmail.com>
2014-12-19 07:23:29 +0900

    編集したテキスト要素をHTMLコンテンツに反映させる処理を実装

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
+
   new TodoFormView($('.todoForm'));
   new TodoListView($('.todoList'));
   $("#subTitle").click(function(e){
-    var textarea, offset;
+    var textarea, savebutton ,offset;
 	  console.log(this.innerHTML);
     offset = $(this).offset();
     console.log(offset);
@@ -17,8 +18,36 @@ $(document).ready(function() {
       fontFamily: "'Open Sans', Helvetica, Arial, sans-serif",
       zIndex: 1,
       background: 'rgba(255,255,255,0.9)'
-    }).val(this.innerHTML);
-    return $('body').append(textarea);
+    }).val(this.innerHTML.replace(/<br\/?>/g, '\n'));
+    
+    savebutton = $('<div id="save" class="btn">').css({
+      display: 'block',
+      position: 'absolute',
+      left: offset.left,
+      top: offset.top + 110,
+      width: 100,
+      height: 30,
+      fontSize: 14,
+      fontFamily: "'Open Sans', Helvetica, Arial, sans-serif",
+      zIndex: 1,
+      background: 'rgba(92,184,92,0.9)',
+      color: '#f9f9f9',
+      cursor: 'pointer'
+    }).text('save');
+    savebutton.on('click', function() {
+      var val;
+      val = textarea.val().replace(/\n/g, '<br>');
+      
+      console.log(val);
+      textarea.remove();
+      this.remove();
+      $("#subTitle").html(val);
+      return;
+    });
+    $('body').append(savebutton);
+    $('body').append(textarea);
+    return;
+
 
   });
   // var $form = $('.todoForm');
