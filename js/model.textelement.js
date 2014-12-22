40e68f894cfad6f3e325797680aff2ac4ab3754e
HiroshiOyamada <h5y1m141@gmail.com>
2014-12-22 09:22:06 +0900

    ModelをJavaScriptの今風のクラス定義に実装し直す

--- js/model.textelement.js
+++ js/model.textelement.js
@@ -1,21 +1,26 @@
-function Todo(data){
-  this.text = data.text;
-  this.complete = !!data.complete;
-}
+var Todo;
+Todo = (function(){
+  function Todo(data) {
+    this.text = data.text;
+    this.complete = !!data.complete;
+  }
+  Todo.prototype.setComplete =function(complete){
+    this.complete = !!complete;
+    this.trigger('change:complete', this);
+  };
+
+  Todo.list = [];
+
+  Todo.add = function(text){
+    var todo;
+    todo = new Todo({ text:text });
+    Todo.list.push(todo);
+    this.trigger('add', todo);
+  };
+
+  return Todo;
+})();
 
 $.extend(Todo.prototype, Backbone.Events);
 $.extend(Todo, Backbone.Events);
-
-Todo.prototype.setComplete =function(complete){
-  this.complete = !!complete;
-  this.trigger('change:complete', this);
-};
-
-Todo.list = [];
-
-Todo.add = function(text){
-  var todo;
-  todo = new Todo({ text:text });
-  Todo.list.push(todo);
-  this.trigger('add', todo);
-};
+console.log(Todo.add);
